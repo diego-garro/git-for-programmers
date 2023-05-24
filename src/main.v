66348ddf0775ab89d5d4f05ca7a215fd428d0332
diego-garro <diego.garromolina@yahoo.com>
2023-05-24 17:21:19 -0600

    Add class Book

--- src/main.v
+++ src/main.v
@@ -1,7 +1,16 @@
 module main
 
+import time
+
 fn main() {
 	println('Hello World!')
 	println('I just added this in VSCode')
 	println('This line added to show the staging area')
+}
+
+struct Book {
+	pub:
+		title string
+		authors string[]
+		publication_date time.Time
 }
